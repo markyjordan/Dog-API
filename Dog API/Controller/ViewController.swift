88c2d748d24e65cbf1f36d2d44f25a034be4828a
Mark Jordan <markybjordan@gmail.com>
2020-08-02 14:22:19 -0700

    Created a struct to decode JSON data with Codable.

--- Dog API/Controller/ViewController.swift
+++ Dog API/Controller/ViewController.swift
@@ -15,6 +15,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // generate image request
+        let randomImageEndpoint = DogAPI.Endpoint.randomImageForAllDogsCollection.url
+        
+        // create URLSessionDataTask
+        let task = URLSession.shared.dataTask(with: randomImageEndpoint) { (data, response, error) in
+            
+            // confirm data received back as response is not nil
+            guard let data = data else {
+                return
+            }
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let url = json["message"] as! String
+                print(url)
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
     }
 
 
---
+++ Dog API/Model/DogImage.swift
@@ -0,0 +1,15 @@
+//
+//  DogImage.swift
+//  Dog API
+//
+//  Created by Marky Jordan on 8/2/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+struct DogImage: Codable {
+    
+    let status: String
+    let message: String
+}
