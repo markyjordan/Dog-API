283bf0df03d7ee61c68956e6da7b1ca1dc7b268d
Mark Jordan <markybjordan@gmail.com>
2020-08-15 22:53:47 -0700

    Created a model object to hold the breeds list data.

---
+++ Dog API/Model/BreedsListResponse.swift
@@ -0,0 +1,15 @@
+//
+//  BreedsListResponse.swift
+//  Dog API
+//
+//  Created by Marky Jordan on 8/15/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+
+struct BreedsListResponse: Codable {
+    
+    let status: String
+    let meesage: [String: [String]]
+}
